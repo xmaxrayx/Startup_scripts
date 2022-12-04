f03ce9ec5739f8e0d6c7af71d9d0106bf7940f34
xMaxrayx <101531362+xmaxrayx@users.noreply.github.com>
2022-12-04 08:42:54 +0300

    Rename steam start up.bat to Steam_startup.bat

---
+++ Steam_startup.bat
@@ -0,0 +1,2 @@
+timeout /t 30
+start  "" "C:\Program Files (x86)\Steam\steamapps\common\Blender\blender.exe"
--- steam start up.bat
+++
@@ -1,2 +0,0 @@
-timeout /t 30
-start  "" "C:\Program Files (x86)\Steam\steamapps\common\Blender\blender.exe"
